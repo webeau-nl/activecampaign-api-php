d93c7606ca5626033ef6ff3857ad405e15241adf
Simon Verwaard <simon@webeau.nl>
2016-02-24 21:05:02 +0100

    Bye debug troep

--- includes/Connector.class.php
+++ includes/Connector.class.php
@@ -5,6 +5,7 @@ class AC_Connector {
 	public $url;
 	public $api_key;
 	public $output = "json";
+	public $version;
 
 	function __construct($url, $api_key, $api_user = "", $api_pass = "") {
 		// $api_pass should be md5() already
@@ -56,7 +57,9 @@ class AC_Connector {
 	}
 
 	public function curl($url, $params_data = array(), $verb = "", $custom_method = "") {
-		if ($this->version == 1) {
+		$method = '';
+
+        if ($this->version == 1) {
 			// find the method from the URL.
 			$method = preg_match("/api_action=[^&]*/i", $url, $matches);
 			if ($matches) {
@@ -69,14 +72,13 @@ class AC_Connector {
 			$method = $custom_method;
 			$url .= "?api_key=" . $this->api_key;
 		}
-		$debug_str1 = "";
+
 		$request = curl_init();
-		$debug_str1 .= "\$ch = curl_init();\n";
-		curl_setopt($request, CURLOPT_HEADER, 0);
+
+        curl_setopt($request, CURLOPT_HEADER, 0);
 		curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-		$debug_str1 .= "curl_setopt(\$ch, CURLOPT_HEADER, 0);\n";
-		$debug_str1 .= "curl_setopt(\$ch, CURLOPT_RETURNTRANSFER, true);\n";
-		if ($params_data && $verb == "GET") {
+
+        if ($params_data && $verb == "GET") {
 			if ($this->version == 2) {
 				$url .= "&" . $params_data;
 				curl_setopt($request, CURLOPT_URL, $url);
@@ -93,21 +95,14 @@ class AC_Connector {
 				$verb = "GET";
 			}
 		}
-		$debug_str1 .= "curl_setopt(\$ch, CURLOPT_URL, \"" . $url . "\");\n";
-		if ($this->debug) {
-			$this->dbg($url, 1, "pre", "Description: Request URL");
-		}
+
 		if ($verb == "POST" || $verb == "PUT" || $verb == "DELETE") {
 			if ($verb == "PUT") {
 				curl_setopt($request, CURLOPT_CUSTOMREQUEST, "PUT");
-				$debug_str1 .= "curl_setopt(\$ch, CURLOPT_CUSTOMREQUEST, \"PUT\");\n";
 			} elseif ($verb == "DELETE") {
 				curl_setopt($request, CURLOPT_CUSTOMREQUEST, "DELETE");
-				$debug_str1 .= "curl_setopt(\$ch, CURLOPT_CUSTOMREQUEST, \"DELETE\");\n";
 			} else {
-				$verb = "POST";
 				curl_setopt($request, CURLOPT_POST, 1);
-				$debug_str1 .= "curl_setopt(\$ch, CURLOPT_POST, 1);\n";
 			}
 			$data = "";
 			if (is_array($params_data)) {
@@ -154,41 +149,22 @@ class AC_Connector {
 
 			$data = rtrim($data, "& ");
 			curl_setopt($request, CURLOPT_HTTPHEADER, array("Expect:"));
-			$debug_str1 .= "curl_setopt(\$ch, CURLOPT_HTTPHEADER, array(\"Expect:\"));\n";
-			if ($this->debug) {
-				curl_setopt($request, CURLINFO_HEADER_OUT, 1);
-				$debug_str1 .= "curl_setopt(\$ch, CURLINFO_HEADER_OUT, 1);\n";
-				$this->dbg($data, 1, "pre", "Description: POST data");
-			}
+
 			curl_setopt($request, CURLOPT_POSTFIELDS, $data);
-			$debug_str1 .= "curl_setopt(\$ch, CURLOPT_POSTFIELDS, \"" . $data . "\");\n";
 		}
 		curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($request, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
-		$debug_str1 .= "curl_setopt(\$ch, CURLOPT_SSL_VERIFYPEER, false);\n";
-		$debug_str1 .= "curl_setopt(\$ch, CURLOPT_SSL_VERIFYHOST, 0);\n";
-		$debug_str1 .= "curl_setopt(\$ch, CURLOPT_FOLLOWLOCATION, true);\n";
+        curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
+
 		$response = curl_exec($request);
-		$debug_str1 .= "curl_exec(\$ch);\n";
-		if ($this->debug) {
-			$this->dbg($response, 1, "pre", "Description: Raw response");
-		}
+
 		$http_code = curl_getinfo($request, CURLINFO_HTTP_CODE);
-		$debug_str1 .= "\$http_code = curl_getinfo(\$ch, CURLINFO_HTTP_CODE);\n";
-		if ($this->debug) {
-			$this->dbg($http_code, 1, "pre", "Description: Response HTTP code");
-			$request_headers = curl_getinfo($request, CURLINFO_HEADER_OUT);
-			$debug_str1 .= "\$request_headers = curl_getinfo(\$ch, CURLINFO_HEADER_OUT);\n";
-			$this->dbg($request_headers, 1, "pre", "Description: Request headers");
-		}
+
 		curl_close($request);
-		$debug_str1 .= "curl_close(\$ch);\n";
+
 		$object = json_decode($response);
-		if ($this->debug) {
-			$this->dbg($object, 1, "pre", "Description: Response object (json_decode)");
-		}
-		if ( !is_object($object) || (!isset($object->result_code) && !isset($object->succeeded) && !isset($object->success)) ) {
+
+		if (!is_object($object) || (!isset($object->result_code) && !isset($object->succeeded) && !isset($object->success)) ) {
 			// add methods that only return a string
 			$string_responses = array("tracking_event_remove", "contact_list", "form_html", "tracking_site_status", "tracking_event_status", "tracking_whitelist", "tracking_log", "tracking_site_list", "tracking_event_list");
 			if (in_array($method, $string_responses)) {
@@ -196,10 +172,6 @@ class AC_Connector {
 			}
 			// something went wrong
 			return "An unexpected problem occurred with the API request. Some causes include: invalid JSON or XML returned. Here is the actual response from the server: ---- " . $response;
-		}
-
-		if ($this->debug) {
-			echo "<textarea style='height: 300px; width: 600px;'>" . $debug_str1 . "</textarea>";
 		}
 
 		header("HTTP/1.1 " . $http_code);
@@ -222,5 +194,3 @@ class AC_Connector {
 	}
 
 }
-
-?>
